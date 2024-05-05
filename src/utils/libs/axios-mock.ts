ba2b7264be9bf3a400551921c03ba96a26d40f8a
reihanpraja <reiandika10@gmail.com>
2024-05-05 22:26:23 +0700

    chore: setup libs

---
+++ src/utils/libs/axios-mock.ts
@@ -0,0 +1,65 @@
+import axios from "axios";
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+export const apiMock = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+  withCredentials: false,
+});
+
+apiMock.interceptors.request.use(function (config) {
+  return config;
+});
+
+apiMock.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    if (error.response?.data.message) {
+      return Promise.reject({
+        ...error,
+        response: {
+          ...error.response,
+          data: {
+            ...error.response.data,
+            message:
+              typeof error.response.data.message === "string"
+                ? error.response.data.message
+                : "Erro inesperado",
+          },
+        },
+      });
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiMock;
+
+type QueryKey = {
+  queryKey: [string];
+};
+
+export const mockQuery = async ({ queryKey }: QueryKey) => {
+  const [url] = queryKey;
+
+  try {
+    const { data } = await apiMock.get(url);
+    return data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+
+export const mockMutation = async (url: string, data: object, type: 'post' | 'put' | 'delete') => {
+  try {
+    const { data: response } = await apiMock[type](url, data);
+    return response;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
